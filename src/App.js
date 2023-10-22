26a7c3f79834d6a52d7435985bbe07b716764245
matstmr <matstmr@gmail.com>
2023-10-22 18:36:24 +0200

    Ajout de mes fichiers de la quete-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+import iaImage from './ia.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <img src={iaImage} alt="" />
+      <h3 className="categorie"><span>habilitado</span></h3>
+      <h2 className="titre">Valores predeterminados
+        en formulario</h2>
+      <p>Formulario de campos con
+        valores predeterminados</p>
+
+    <span className="menu">...</span>
     </div>
   );
 }
